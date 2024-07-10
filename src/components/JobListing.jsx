815c6ee5dbfbca0d53b0ba9d4d8b8b245f6bb463
Adel <110723457+Addysd@users.noreply.github.com>
2024-07-10 23:07:24 +0530

    Update JobListing.jsx

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -31,7 +31,7 @@ const JobListing = ({job}) => {
                     {job.location}
                   </div>
                   <Link
-                    to={`https://json-server-7xnc.onrender.com/jobs/${job.id}`}
+                    to={`/jobs/${job.id}`}
                     className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                   >
                     Read More
