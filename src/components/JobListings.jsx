043c3e4cd0dc5a684f1d2c0f4b3019b47bff3b55
Adel <110723457+Addysd@users.noreply.github.com>
2024-07-10 22:42:49 +0530

    Update JobListings.jsx

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -3,18 +3,16 @@ import JobListing from './JobListing';
 import PropTypes from 'prop-types'; 
 import Spinner from './Spinner';
 
-const JobListings = ({isHome=false}) => {
-    const [jobs,setJobs]=useState([]);
-    const [loading,setLoading]=useState(true);
+const JobListings = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(()=>{
-      const fetchJobs= async () => {
-        const apiUrl= isHome
-        ? '/api/jobs?_limit=3'
-        : '/api/jobs';
-        try{
-          const res=await fetch(apiUrl);
-        const data=  await res.json();
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs';
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
         if (Array.isArray(data)) {
           setJobs(data);
         } else {
@@ -25,30 +23,33 @@ const JobListings = ({isHome=false}) => {
       } finally {
         setLoading(false);
       }
-        
-      };
-      fetchJobs();
-    },[]);
+    };
+    fetchJobs();
+  }, [isHome]);
+
   return (
     <section className="bg-blue-50 px-4 py-10">
-        <div className="container-xl lg:container m-auto">
-          <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-            {isHome?'Recent Jobs':'Browse Jobs'}
-          </h2>
-          
-            {loading? (<Spinner loading={loading}/>):(
-             <div className="grid grid-cols-1 md:grid-cols-3 gap-6"> 
-              {jobs.map((job)=>
-            (
-                <JobListing key={job.id} job={job} />
+      <div className="container-xl lg:container m-auto">
+        <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+          {isHome ? 'Recent Jobs' : 'Browse Jobs'}
+        </h2>
+
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
             ))}
-            </div>
-            )}
-        </div>
-      </section>
-  )
+          </div>
+        )}
+      </div>
+    </section>
+  );
 }
+
 JobListings.propTypes = {
   isHome: PropTypes.bool,
 };
-export default JobListings
+
+export default JobListings;
